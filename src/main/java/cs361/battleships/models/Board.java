8506de6233dea4ca208c030c764f81ed266182e0
trumanhess <46516842+trumanhess@users.noreply.github.com>
2019-01-31 14:56:28 -0800

    Revert "Added the working code for Sprint 2"

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -1,77 +1,183 @@
 package cs361.battleships.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.loader.custom.sql.SQLQueryReturnProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Board {
-
-	@JsonProperty private List<Ship> ships;
-	@JsonProperty private List<Result> attacks;
-
-	/*
-	DO NOT change the signature of this method. It is used by the grading scripts.
-	 */
-	public Board() {
-		ships = new ArrayList<>();
-		attacks = new ArrayList<>();
-	}
+public class Board
+{
+	private List<Ship> totalShips;
+	private List<Result> totalAtacks;
+	private boolean[][] boardLayout;	// for identifying occupied spaces
+	final int BOARDX = 10,
+			  BOARDY = BOARDX;
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
-	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
-		if (ships.size() >= 3) {
-			return false;
-		}
-		if (ships.stream().anyMatch(s -> s.getKind().equals(ship.getKind()))) {
-			return false;
-		}
-		final var placedShip = new Ship(ship.getKind());
-		placedShip.place(y, x, isVertical);
-		if (ships.stream().anyMatch(s -> s.overlaps(placedShip))) {
-			return false;
-		}
-		if (placedShip.getOccupiedSquares().stream().anyMatch(s -> s.isOutOfBounds())) {
-			return false;
-		}
-		ships.add(placedShip);
-		return true;
-	}
-
-	/*
-	DO NOT change the signature of this method. It is used by the grading scripts.
-	 */
-	public Result attack(int x, char y) {
-		Result attackResult = attack(new Square(x, y));
-		attacks.add(attackResult);
-		return attackResult;
-	}
-
-	private Result attack(Square s) {
-		if (attacks.stream().anyMatch(r -> r.getLocation().equals(s))) {
-			var attackResult = new Result(s);
-			attackResult.setResult(AtackStatus.INVALID);
-			return attackResult;
-		}
-		var shipsAtLocation = ships.stream().filter(ship -> ship.isAtLocation(s)).collect(Collectors.toList());
-		if (shipsAtLocation.size() == 0) {
-			var attackResult = new Result(s);
-			return attackResult;
-		}
-		var hitShip = shipsAtLocation.get(0);
-		var attackResult = hitShip.attack(s.getRow(), s.getColumn());
-		if (attackResult.getResult() == AtackStatus.SUNK) {
-			if (ships.stream().allMatch(ship -> ship.isSunk())) {
-				attackResult.setResult(AtackStatus.SURRENDER);
+	public Board()
+	{
+		totalShips = new ArrayList();
+		totalAtacks = new ArrayList();
+		boardLayout = new boolean[BOARDX][BOARDY];
+		// setting up the board as a blank slate
+		for (int i = 0; i < BOARDX; i++) {
+			for (int j = 0; j < BOARDY; j++) {
+				boardLayout[i][j] = false;
 			}
 		}
-		return attackResult;
 	}
 
-	List<Ship> getShips() {
-		return ships;
+	/*
+	DO NOT change the signature of this method. It is used by the grading scripts.
+	 */
+	public boolean placeShip(Ship ship, int x, char y, boolean isVertical)
+	{
+		// TODO Implement
+		Square s;
+		int xoff;
+		char yoff;
+
+		for(Ship sType: totalShips) {
+			if(sType.getsName().equals(ship.getsName())) {
+				return false;
+			} else if ((x < 0 || x > 10) || (y < 'A' || y > 'J')) {
+				return false;
+			}
+		}
+
+		if(isVertical) {
+			for (int i = 0; i < ship.getSize(); i++) {
+				s = new Square((x + i), y);
+				if ((x + i) < 1 || (x + 1) > 10) {
+					return false;
+				} else if (getBoardLayout(x,y) == true) {
+					return false;
+				} else {
+					ship.setOccupiedSquares(s);
+					setBoardLayout(x,y,true);
+				}
+			}
+		} else {
+			for(int i = 0; i < ship.getSize(); i++)
+			{
+				if ((char)((int)y+i) > 'A' || (char)((int)y+i) < 'J') {
+					return false;
+				} else if (getBoardLayout(x,y) == true) {
+					return false;
+				}
+				else {
+					s = new Square(x, (char)((int)y+i));
+					ship.setOccupiedSquares(s);
+					setBoardLayout(x,y,true);
+
+				}
+			}
+		}
+
+
+
+		for(int i = 0; i < ship.getOccupiedSquares().size(); i++)
+		{
+			xoff = ship.getOccupiedSquares().get(i).getRow();
+			yoff = ship.getOccupiedSquares().get(i).getColumn();
+
+			if((xoff > 0 && xoff < 11) && (yoff >= 'A' && yoff <= 'J'))
+			{
+				totalShips.add(ship);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	DO NOT change the signature of this method. It is used by the grading scripts.
+	 */
+	public Result attack(int x, char y)
+	{
+		//TODO Implement
+		Result r = new Result();
+		Square s = new Square(x, y);
+		r.setLocation(s);
+
+		if((x > 0 && x < 11) &&(y >= 'A' && y <= 'J'))
+		{
+			for(Ship ship : totalShips)
+			{
+				for (Square square : ship.getOccupiedSquares())
+				{
+					if(square.getRow() == x && square.getColumn() == y)
+					{
+						r.setResult(AtackStatus.HIT);
+						ship.setsHealth();
+						if(ship.getsHealth() == 0)
+						{
+							r.setResult(AtackStatus.SUNK);
+							r.setShip(new Ship(ship.getsName()));
+							if(r.getShipCount() == 3)
+							{
+								r.setResult(AtackStatus.SURRENDER);
+							}
+						}
+					}
+					else
+					{
+						r.setResult(AtackStatus.MISS);
+					}
+				}
+			}
+		}
+		else
+		{
+			r.setResult(AtackStatus.INVALID);
+		}
+
+		if(totalAtacks.isEmpty())
+        {
+            totalAtacks.add(r);
+        }
+		else {
+            for (int i = 0; i < totalAtacks.size(); i++) {
+                if (totalAtacks.get(i).getLocation() == r.getLocation()) {
+                    r.setResult(AtackStatus.INVALID);
+                }
+            }
+        }
+		totalAtacks.add(r);
+
+		return r;
+	}
+
+	public boolean getBoardLayout(int xCoo, int yCoo) {
+		return boardLayout[xCoo][yCoo];
+	}
+	public void setBoardLayout(int xCoo, int yCoo, boolean state) {
+		boardLayout[xCoo][yCoo] = state;
+	}
+
+	public List<Ship> getShips()
+	{
+		//TODO implement
+		return this.totalShips;
+	}
+
+	public void setShips(List<Ship> ships)
+	{
+		//TODO implement
+		this.totalShips = ships;
+	}
+
+	public List<Result> getAttacks()
+	{
+		//TODO implement
+		return this.totalAtacks;
+	}
+
+	public void setAttacks(List<Result> attacks)
+	{
+		//TODO implement
+		this.totalAtacks = attacks;
 	}
 }
